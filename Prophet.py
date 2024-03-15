ea56ed02af1a5ed4a996087f10fae35988f5056d
SonTDc <wnos278@gmail.com>
2024-03-15 23:59:04 +0700

    Init

---
+++ Prophet.py
@@ -0,0 +1,21 @@
+# class là một tiên tri nhận đầu vào là dữ liệu timeseries và đưa ra phỏng đoán tiếp theo về sự kiện 
+
+class Prophet:
+
+    def __init__(self) -> None:
+        pass
+
+    # Đưa ra dữ kiện đầu vào 
+    def load_data(self): # Input
+        pass 
+
+
+    # Đưa ra dự báo
+    def forecast(self):
+        pass  
+
+        
+    # Đưa ra báo cáo 
+    def export(self):
+        pass 
+
